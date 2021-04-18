e8c17b19318f0f683a56d56c94f970aae763f3c6
Ivan Arriola <navis462@gmail.com>
2021-04-18 00:46:39 -0600

    Second assignment 'color assignment, input outout'

---
+++ FavoriteColor.py
@@ -0,0 +1,16 @@
+
+
+favColor = input("Please type your favorite color: ")
+print("Your favorite color is\n", favColor)
+
+hobby = input("What is your favorite hobby: ")
+
+travel = input("Where would you like to travel: ")
+
+name = input("What is your name: ")
+
+print()
+print("So you are", name, "who likes", hobby, "and would like to travel to", travel + "?")
+
+print()
+print(" *southern accent* Well I'll be, good for you son.")
