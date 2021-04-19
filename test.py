e7fbf6b708a2e537ce5cbfcd9df0284b83cd0c82
Ivan Arriola <navis462@gmail.com>
2021-04-19 02:32:32 -0600

    escape

---
+++ test.py
@@ -0,0 +1,18 @@
+
+
+
+hairColor = input("Please type your hair color: ")
+
+eyeColor = input("Please type your eye color: ")
+
+startMonth = input("Please type month that you started working: ")
+
+advTraining = input("Have you recieved advanced training (type yes or no): ")
+
+
+
+print(f"{'Hair: ' + hairColor.capitalize():<25} Eyes: {eyeColor.capitalize()}")
+
+print(f"{'Month: ' + startMonth.capitalize():<25} Training: {advTraining.capitalize()}")
+
+print("-------------------------------")
