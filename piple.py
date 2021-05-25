86ff1b982711e28ea86642ae558175736641b0aa
Ivan Arriola <navis462@gmail.com>
2021-05-24 20:56:34 -0600

    Python priple cert

---
+++ piple.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+for index in range (1,101):
+    if index % 3 == 0 and index % 5 == 0:
+        print("FizzBuzz")
+    elif index % 5 == 0:
+        print("Buzz")
+
+    if index % 3 == 0:
+        print("Fizz")
+    else:
+        print(index)
